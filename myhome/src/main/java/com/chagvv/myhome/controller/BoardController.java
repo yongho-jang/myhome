ddfbdd9bc960d35533912b15631a5558afb82dc6
chagvv <chagvv@naver.com>
2021-01-06 01:12:57 +0900

    6강 게시판 생성 수정 validation 처리

--- myhome/src/main/java/com/chagvv/myhome/controller/BoardController.java
+++ myhome/src/main/java/com/chagvv/myhome/controller/BoardController.java
@@ -2,14 +2,21 @@ package com.chagvv.myhome.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.chagvv.myhome.model.Board;
 import com.chagvv.myhome.repository.BoardRepository;
+import com.chagvv.myhome.validator.BoardValidator;
 
 @Controller
 @RequestMapping("/board")
@@ -18,11 +25,39 @@ public class BoardController {
 	@Autowired
 	private BoardRepository boardRepository;
 	
+	@Autowired
+	private BoardValidator boardValidatory;
+	
 	@GetMapping("/list")
 	public String list(Model  model) {
 		
 		List<Board> boards = boardRepository.findAll();
 		model.addAttribute("boards", boards);
 		return "board/list";
+	}
+	
+	@GetMapping("/form")
+	public String form(Model  model, @RequestParam(required = false) Long id) {
+		
+		if(id==null) {
+			model.addAttribute("board", new Board());
+		}else {
+			Board board = boardRepository.findById(id).orElse(null);
+			model.addAttribute("board", board);
+		}
+		return "board/form";
+	}
+	
+	@PostMapping("/form")
+	public String formSubmit(@Valid Board board, BindingResult bindingResult) {
+		
+		boardValidatory.validate(board, bindingResult);
+		
+		if (bindingResult.hasErrors()) {
+			return "board/form";
+		}
+		
+		boardRepository.save(board);
+		return "redirect:/board/list";
 	}
 }
--- myhome/src/main/java/com/chagvv/myhome/model/Board.java
+++ myhome/src/main/java/com/chagvv/myhome/model/Board.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 import lombok.Data;
 
@@ -15,6 +18,8 @@ public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotNull
+	@Size(min=2, max=30, message = "제목은 2자 이상 입력 하세요!")
 	private String title;
 	@Lob
 	private String content;
